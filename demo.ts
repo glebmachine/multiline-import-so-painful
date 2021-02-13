1606f0e2c84f90ee46463a7e41be9e780a5e6ea9
Gleb Mikheev <glebmachine@gmail.com>
2021-02-13 16:25:03 +0300

    2

--- demo.ts
+++ demo.ts
@@ -1,9 +1,13 @@
 import { 
-  NgModule,
-  Component,
-  Directive
-} from '@angular/core';
+  ajax,
+  api,
+} from 'jquery';
 import { 
   HttpClientModule,
   HttpClient,
 } from '@angular/common';
+import { 
+  NgModule,
+  Component,
+  Directive
+} from '@angular/core';
